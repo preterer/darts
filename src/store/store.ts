2d8f181e58282038828dd5dfaf8d1da24804c23b
Michal <preterer@gmail.com>
2019-11-28 18:49:03 +0100

    storeState interface

--- src/store/store.ts
+++ src/store/store.ts
@@ -10,12 +10,38 @@ import { players } from "./modules/players.module";
 
 Vue.use(Vuex);
 
-export const store = new Store<{
-  calculation: {
-    service: DartsService;
-  };
+/**
+ * State of the store
+ *
+ * @export
+ * @interface StoreState
+ */
+export interface StoreState {
+  /**
+   * Calculation module state
+   *
+   * @type {{ service: DartsService }}
+   * @memberof StoreState
+   */
+  calculation: { service: DartsService };
+
+  /**
+   * Game module state
+   *
+   * @type {GameWithHistory}
+   * @memberof StoreState
+   */
   game: GameWithHistory;
+
+  /**
+   * Players module state
+   *
+   * @type {{ list: Player[] }}
+   * @memberof StoreState
+   */
   players: { list: Player[] };
-}>({
+}
+
+export const store = new Store<StoreState>({
   modules: { calculation, game, players }
 });
