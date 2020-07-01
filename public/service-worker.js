c68be0e38d0317b396cc033a1d51001ef872775b
Michal Baranowski <preterer@gmail.com>
2020-07-01 22:42:56 +0200

    Fixed cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", function(event) {
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open("darts-cache").then(function(cache) {
       return cache.addAll([
         "/",
         "/index.html",
