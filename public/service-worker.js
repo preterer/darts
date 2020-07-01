b1c39fd7007997f3085bca65bfcfbb8679d610c7
Michal Baranowski <preterer@gmail.com>
2020-07-01 22:37:01 +0200

    Caching index

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,6 +2,7 @@ self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll([
+        "/index.html",
         "/css/bootstrap.css",
         "/css/styles.css",
         "/dist/build.js",
