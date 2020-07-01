4fbb0bcb0901c7b2d32ab6b96d874d6713b4c63b
Michal Baranowski <preterer@gmail.com>
2020-07-01 22:41:40 +0200

    More caching and cache usage

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,12 +2,21 @@ self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll([
+        "/",
         "/index.html",
         "/css/bootstrap.css",
         "/css/styles.css",
         "/dist/build.js",
         "/dist/dart.svg",
       ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
