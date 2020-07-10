34cd3c4ce3110460cbaca9a1a8992ea652ff98f0
Michal Baranowski <preterer@gmail.com>
2020-07-10 18:44:14 +0200

    Cache fixes

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./dist"),
     publicPath: "/dist/",
-    filename: "build.js"
+    filename: "build.js",
   },
   module: {
     rules: [
@@ -20,54 +20,47 @@ module.exports = {
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
             scss: "vue-style-loader!css-loader!sass-loader",
-            sass: "vue-style-loader!css-loader!sass-loader?indentedSyntax"
-          }
+            sass: "vue-style-loader!css-loader!sass-loader?indentedSyntax",
+          },
           // other vue-loader options go here
-        }
+        },
       },
       {
         test: /\.tsx?$/,
         loader: "ts-loader",
         exclude: /node_modules/,
         options: {
-          appendTsSuffixTo: [/\.vue$/]
-        }
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: "file-loader",
-        options: {
-          name: "[name].[ext]?[hash]"
-        }
+          appendTsSuffixTo: [/\.vue$/],
+        },
       },
       {
         test: /\.css$/,
-        use: ["vue-style-loader", "css-loader"]
+        use: ["vue-style-loader", "css-loader"],
       },
       {
         test: /.html$/,
-        use: ["html-loader"]
-      }
-    ]
+        use: ["html-loader"],
+      },
+    ],
   },
   resolve: {
     extensions: [".ts", ".js", ".vue", ".json"],
     alias: {
-      vue$: "vue/dist/vue.esm.js"
-    }
+      vue$: "vue/dist/vue.esm.js",
+    },
   },
   devServer: {
     contentBase: path.join(__dirname, "public"),
-    port: 9000
+    port: 9000,
   },
   performance: {
-    hints: false
+    hints: false,
   },
   devtool: "#eval-source-map",
   plugins: [
     // make sure to include the plugin for the magic
-    new VueLoaderPlugin()
-  ]
+    new VueLoaderPlugin(),
+  ],
 };
 
 if (process.env.NODE_ENV === "production") {
@@ -76,17 +69,17 @@ if (process.env.NODE_ENV === "production") {
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       "process.env": {
-        NODE_ENV: '"production"'
-      }
+        NODE_ENV: '"production"',
+      },
     }),
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
       compress: {
-        warnings: false
-      }
+        warnings: false,
+      },
     }),
     new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
+      minimize: true,
+    }),
   ]);
 }
